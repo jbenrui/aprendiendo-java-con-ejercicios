56018f28ceca1ad0be5fb3ae74fea958c990027f
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-14 10:44:31 +0200

    Ejercicio 2 del tema 2

---
+++ TEMA-2/JabrEjercicio02Tema2.java
@@ -0,0 +1,19 @@
+/*
+ * Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por 
+ * pantalla de tal forma que el resultado del programa sea el mismo que en el
+ * ejercicio 1 del capítulo 1.
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+
+
+public class JabrEjercicio02Tema2 {
+	
+	public static void main (String[] args) {
+		String nombre = "Jose Antonio Benitez Ruiz";
+    System.out.println(nombre);
+	}
+}
+
