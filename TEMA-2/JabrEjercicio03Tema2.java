2c1b684987a38fff6cc66caf930766b13d31ff81
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-14 10:56:14 +0200

    Ejercicio 3 del tema 2

---
+++ TEMA-2/JabrEjercicio03Tema2.java
@@ -0,0 +1,20 @@
+/*
+ * Crea las variables nombre, direccion y telefono y asígnales los valores correspon
+ * dientes. Muestra los valores de esas variables por pantalla de tal forma que el
+ * resultado del programa sea el mismo que en el ejercicio 2.
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+
+
+public class JabrEjercicio03Tema2 {
+	
+	public static void main (String[] args) {
+		String nombre = "Jose Antonio Benitez Ruiz";
+    String direccion = "C/ Frederik Terman n3";
+    String telefono = "717707879";
+    System.out.printf("Mi nombre es %s, estudio en %s y mi telefono es %s \n" ,nombre ,direccion ,telefono);
+	}
+}
