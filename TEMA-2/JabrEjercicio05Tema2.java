1b387d78ca95588290b8b378be872e32360423fa
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-14 11:13:44 +0200

    Ejercicio5 del tema 2

---
+++ TEMA-2/JabrEjercicio05Tema2.java
@@ -0,0 +1,20 @@
+/*
+ * Realiza un conversor de euros a pesetas. La cantidad en euros que se quiere
+ * convertir deberá estar almacenada en una variable.
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+
+
+public class JabrEjercicio05Tema2 {
+	
+	public static void main (String[] args) {
+		double valorConvertir = 831.90;
+    double valorPeseta = 166.38;
+    
+    double resultado = (valorConvertir / valorPeseta);
+    System.out.printf(" %.2f pesetas en euros son: %.2f €" ,valorConvertir ,resultado);
+	}
+}
