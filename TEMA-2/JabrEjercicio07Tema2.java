da78ef5d119e04888e9c9644c704dfc7af53427c
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-16 14:55:09 +0200

    Ejercicio 7 del tema 2

---
+++ TEMA-2/JabrEjercicio07Tema2.java
@@ -0,0 +1,19 @@
+/*
+ * Escribe un programa que declare variables de tipo char y de tipo String. Intenta
+ * mostrarlas por pantalla todas juntas en la misma línea y con una sola sentencia
+ * de Java (con un solo println) ¿es posible?
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+
+
+public class JabrEjercicio07Tema2 {
+	public static void main (String[] args) {
+		char inicial = 'J';
+    String nombreCompleto = "Jose Antonio Benitez Ruiz";
+    System.out.println("No, no es posible, seria posible con el printf");
+	}
+}
+
