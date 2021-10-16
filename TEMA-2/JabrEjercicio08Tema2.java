add11fd182dbbbe170c0f76f54308fb05771c0c4
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-16 16:01:14 +0200

    Ejercicio 8 del tema 2

---
+++ TEMA-2/JabrEjercicio08Tema2.java
@@ -0,0 +1,28 @@
+/*
+ * Escribe un programa que declare 5 variables de tipo char. A continuación, crea
+ * otra variable como cadena de caracteres y asígnale como valor la concatenación 
+ * de las anteriores 5 variables. Por último, muestra la cadena de caracteres
+ * por pantalla ¿Qué problemas te encuentras? ¿cómo lo has solucionado?
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+
+
+public class JabrEjercicio08Tema2 {
+	public static void main (String[] args) {
+    char letraJ = 'J';
+    char letraO = 'o';
+    char letraS = 's';
+    char letraE = 'e';
+
+    // La sigiente linea da error si intentamos sumar la variables
+    // La solucion es añadirle unas "" como si estuviesemos sumando numeros 
+    // enteros
+    String nombreCompleto = "" + letraJ + letraO + letraS + letraE;
+    
+    System.out.println(nombreCompleto);
+	}
+}
+
