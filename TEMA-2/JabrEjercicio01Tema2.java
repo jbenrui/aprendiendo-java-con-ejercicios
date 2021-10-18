22f339d81a21267aef79c27d9d9e9bec553fcfa7
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-18 22:00:40 +0200

    Ejercicio 1 Tema 2

--- TEMA-2/JabrEjercicio01Tema2.java
+++ TEMA-2/JabrEjercicio01Tema2.java
@@ -1,5 +1,7 @@
 /*
- * Uso de variables enteras
+ * Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+ * los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+ * el valor de cada variable, la suma, la resta, la división y la multiplicación
  * 
  * @author Jose Antonio Benitez Ruiz
  * 
