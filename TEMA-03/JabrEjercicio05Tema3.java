11890b8ca844aaa5600febe41b69c13cf76fb2d2
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-18 22:21:50 +0200

    Ejercicio 5 Tema 3

---
+++ TEMA-03/JabrEjercicio05Tema3.java
@@ -0,0 +1,22 @@
+/*
+ * Escribe un programa que calcule el área de un rectángulo.
+ * 
+ * @author: Jose Antonio Benitez Ruiz
+ * 
+ */
+
+
+public class JabrEjercicio05Tema3{
+  
+  public static void main (String[] args) {
+    System.out.print("Introduce la base del rectangulo: ");
+    int base = Integer.parseInt(System.console().readLine() );
+    System.out.print("Introduce la altura del rectangulo: ");
+    int altura = Integer.parseInt(System.console().readLine() );
+    
+    int resultado = base * altura;
+    System.out.printf("El área de un rectángulo es : %d" ,resultado );
+    
+  }
+}
+
