e7b879c43d3a0c1b4b32b71dfc8cec39b44ae2c6
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-19 08:56:32 +0200

    Ejercicio 2 del tema 3 actualizado

--- TEMA-03/JabrEjercicio02Tema3.java
+++ TEMA-03/JabrEjercicio02Tema3.java
@@ -1,5 +1,5 @@
-/*
- * Realiza un conversor de euros a pesetas. La cantidad de euros que se quiere
+/**
+ * Realiza un conversor de pesetas a euros. La cantidad de euros que se quiere
  * convertir debe ser introducida por teclado.
  * 
  * @author Jose Antonio Benitez Ruiz
@@ -11,10 +11,10 @@
 public class JabrEjercicio02Tema3 {
   public static void main (String[] args) {
     System.out.print("Introduce el valor de pesetas que quieres conventir: ");
-    double valorConvertir = Double.parseDouble(System.console().readLine() );;
-    double valorPeseta = 166.38;
+    int pesetas = Integer.parseInt(System.console().readLine() );;
+    double factorConversion = 166.38;
     
-    double resultado = (valorConvertir / valorPeseta);
-    System.out.printf(" %.2f pesetas en euros son: %.2f €" ,valorConvertir ,resultado);
+    double euros = (pesetas / factorConversion);
+    System.out.printf(" %d pesetas en euros son: %.2f €" ,pesetas ,euros);
  }
 }
