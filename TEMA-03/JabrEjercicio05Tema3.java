7b3f29fae1124d0a9077f1ca49d033296c69a09a
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-19 09:06:26 +0200

    Ejercicio 5 del tema 3 actualizado

--- TEMA-03/JabrEjercicio05Tema3.java
+++ TEMA-03/JabrEjercicio05Tema3.java
@@ -8,12 +8,12 @@ public class JabrEjercicio05Tema3{
   
   public static void main (String[] args) {
     System.out.print("Introduce la base del rectangulo en cm: ");
-    int base = Integer.parseInt(System.console().readLine() );
+    double base = Double.parseDouble(System.console().readLine() );
     System.out.print("Introduce la altura del rectangulo en cm: ");
-    int altura = Integer.parseInt(System.console().readLine() );
+    double altura = Double.parseDouble(System.console().readLine() );
     
-    int resultado = base * altura;
-    System.out.printf("El área de un rectángulo es : %d cm" ,resultado );
+    double resultado = base * altura;
+    System.out.printf("El área de un rectángulo es : %.f cm" ,resultado );
     
   }
 }
