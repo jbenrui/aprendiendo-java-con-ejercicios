40ce6707916b3b8db69d0a4f111db73258f66126
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-19 09:11:08 +0200

    Ejercicio 6 del tema 3 actualizado

--- TEMA-03/JabrEjercicio06Tema3.java
+++ TEMA-03/JabrEjercicio06Tema3.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Escribe un programa que calcule el área de un triangulo.
  * 
  * @author: Jose Antonio Benitez Ruiz
@@ -9,13 +9,13 @@
 public class JabrEjercicio06Tema3{
   
   public static void main (String[] args) {
-    System.out.print("Introduce la base del triangulo: ");
-    int base = Integer.parseInt(System.console().readLine() );
-    System.out.print("Introduce la altura del triangulo: ");
-    int altura = Integer.parseInt(System.console().readLine() );
+    System.out.print("Introduce la base del triángulo en cm: ");
+    double base = Double.parseDouble(System.console().readLine() );
+    System.out.print("Introduce la altura del triángulo en cm: ");
+    double altura = Double.parseDouble(System.console().readLine() );
     
     double resultado = (base * altura) / 2;
-    System.out.printf("El área de un rectángulo es : %.2f" ,resultado );
+    System.out.printf("El área de un triángulo es : %.2f" ,resultado );
     
   }
 }
