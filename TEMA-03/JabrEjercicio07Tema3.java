c8c2875b5a00cc83de451121f8a45e5a7e42811c
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-21 08:59:55 +0200

    Actualizado

--- TEMA-03/JabrEjercicio07Tema3.java
+++ TEMA-03/JabrEjercicio07Tema3.java
@@ -8,12 +8,12 @@
  */
 public class JabrEjercicio07Tema3 {
   public static void main (String[] args) {
-    System.out.print("Introduce la base imponible a calcular: ");
+    System.out.print("Introduce la base imponible a calcular (en euros): ");
     double baseImponible = Double.parseDouble(System.console().readLine());
     double iva = 0.21;
     
     double resultado = ( baseImponible * iva ) + baseImponible;
-    System.out.printf("La base imponible es %.2f, con el iva añadidio es: %.2f " ,baseImponible ,resultado);
+    System.out.printf("La base imponible es %.2f €, con el iva añadidio es: %.2f €" ,baseImponible ,resultado);
 	}
 }
 
