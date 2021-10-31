4cd095ce88152700174082ebf0d86d1f43921311
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-10-31 12:23:37 +0100

    Ejercicio 28 Tema 4

---
+++ TEMA-4/JabrEjercicio28Tema4.java
@@ -0,0 +1,40 @@
+/**
+ * Implementa el juego piedra, papel y tijera. Primero, el usuario 1 introduce
+ * su jugada y luego el usuario 2. Si alguno de los usuarios introduce una opción
+ * incorrecta, el programa deberá mostrar un mensaje de error.
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+public class JabrEjercicio28Tema4 {
+  public static void main (String[] args) {
+    System.out.println("El juego de piedra, papel, tijeras.");
+    
+    System.out.print("Turno del primer jugador: (introduce piedra, papel,tijeras): ");
+    String user1 = (System.console().readLine() );
+    System.out.print("Turno del segundo jugador: (introduce piedra, papel,tijeras): ");
+    String user2 = (System.console().readLine() );
+    
+    if ((user1.equals("papel")) && (user2.equals("piedra"))){
+      System.out.print("Gana el jugador 1");
+    } else if ((user1.equals("papel")) && (user2.equals("tijeras"))){
+      System.out.print("Gana el jugador 2");
+    } else if ((user1.equals("tijeras")) && (user2.equals("papel"))){
+      System.out.print("Gana el jugador 1");
+    } else if ((user1.equals("tijeras")) && (user2.equals("piedra"))){
+      System.out.print("Gana el jugador 2");
+    } else if ((user1.equals("piedra")) && (user2.equals("papel"))){
+      System.out.print("Gana el jugador 2");
+    } else if ((user1.equals("piedra")) && (user2.equals("tijeras"))){
+      System.out.print("Gana el jugador 1");
+    } else if ((user1.equals("piedra")) && (user2.equals("piedra"))){
+      System.out.print("Empate");
+    } else if ((user1.equals("tijera")) && (user2.equals("tijera"))){
+      System.out.print("Empate");
+    } else if ((user1.equals("papel")) && (user2.equals("papel"))){
+      System.out.print("Empate");
+    } 
+  }
+}
+
