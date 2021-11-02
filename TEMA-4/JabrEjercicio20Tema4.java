2055a3568abd858c64bb20d98f045b228f2dcb3c
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-11-02 10:44:51 +0100

    Ejercicio 20 Tema 4

--- TEMA-4/JabrEjercicio20Tema4.java
+++ TEMA-4/JabrEjercicio20Tema4.java
@@ -16,39 +16,49 @@ public class JabrEjercicio20Tema4 {
       int digito = 0;
       int ultimaCifra;
       int primeraCifra;
+      int segundaCifra;
+      int terceraCifra;
+      int cuartaCifra;
+      //numero de 2 cifras
       if (((n >= 10)&&(n < 100))||((n <= -10)&&(n > -100))){
         digito = 10;
         ultimaCifra =(n %10);
         primeraCifra =(n / digito);
+        
         if (( primeraCifra == ultimaCifra )&&( ultimaCifra == primeraCifra )){
           System.out.printf("El %d es capicúa",n);
         }else {
           System.out.printf("El %d NO es capicúa",n);
         }
+      //numero de 3 cifras
       } else if (((n >= 100)&&(n < 1000))||((n <= -100)&&(n > -1000))){
-        digito = 100;
         ultimaCifra =(n %10);
-        primeraCifra =(n / digito);
+        primeraCifra =(n / 100);
         if (( primeraCifra == ultimaCifra )&&( ultimaCifra == primeraCifra )){
           System.out.printf("El %d es capicúa",n);
         }else {
           System.out.printf("El %d NO es capicúa",n);
         }
         
+      //numero de 4 cifras
       } else if (((n >= 1000)&&(n < 10000))||((n <= -1000)&&(n > -10000))){
-        digito = 1000;
-        ultimaCifra =(n %10);
-        primeraCifra =(n / digito);
-        if (( primeraCifra == ultimaCifra )&&( ultimaCifra == primeraCifra )){
+        ultimaCifra = n % 10;
+        primeraCifra = n / 1000;
+        segundaCifra = (n / 100) % 10;  
+        terceraCifra = (n /10) % 10;
+        if (( primeraCifra == ultimaCifra )&&( segundaCifra == terceraCifra )){
           System.out.printf("El %d es capicúa",n);
         }else {
           System.out.printf("El %d NO es capicúa",n);
         }
+        
+      //numero de 5 cifras
       } else if (((n >= 10000)&&(n < 100000))||((n <= -10000)&&(n > -100000))){
-        digito = 10000;
         ultimaCifra =(n %10);
-        primeraCifra =(n / digito);
-        if (( primeraCifra == ultimaCifra )&&( ultimaCifra == primeraCifra )){
+        primeraCifra =(n / 10000);
+        segundaCifra = (n / 1000) % 10; 
+        cuartaCifra = (n /10) % 10;
+        if (( primeraCifra == ultimaCifra )&&( segundaCifra == cuartaCifra )){
           System.out.printf("El %d es capicúa",n);
         }else {
           System.out.printf("El %d NO es capicúa",n);
