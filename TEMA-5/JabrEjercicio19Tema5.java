127d1940945c54bf215fb99a1222f8a0c2e85e72
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-11-11 10:45:38 +0100

    Ejercicio 19 TEMA 5

---
+++ TEMA-5/JabrEjercicio19Tema5.java
@@ -0,0 +1,35 @@
+/**
+ * Ejercicio: 19
+ * Realiza un programa que pinte una pirámide por pantalla. La altura se debe pe
+ * -dir por teclado.  El carácter con el que se pinta la pirámide también se debe
+ * pedir por teclado.
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+public class JabrEjercicio19Tema5 {
+  public static void main (String[] args) {
+    System.out.print("Introduce la altura de la piramide por teclado: ");
+    int altura = Integer.parseInt(System.console().readLine()) ;
+    System.out.print("Introduce el caracter por teclado: ");
+    String caracter = System.console().readLine();
+    int base = 1;
+    int numespacio = altura -1;
+    int longitudLinea = 1;
+    while (base <=altura ){
+      
+      for (int a = 1; a <= numespacio; a++){
+        System.out.print(" ");
+        
+      }
+      for (int a = 1; a <= longitudLinea; a++ ){
+        System.out.print(caracter);
+      }
+      System.out.println(" ");
+    base++;
+    numespacio--;
+    longitudLinea+=2;
+    }
+  }
+}
