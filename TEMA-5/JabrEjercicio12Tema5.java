19e72afe0225d762ec2fac5c66d886fccc144c1c
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-11-11 13:33:07 +0100

    Ejercicio 12 TEMA 5

--- TEMA-5/JabrEjercicio12Tema5.java
+++ TEMA-5/JabrEjercicio12Tema5.java
@@ -12,20 +12,30 @@
  */
 public class JabrEjercicio12Tema5 {
   public static void main (String[] args) {
-    System.out.print("Introduce un número por teclado: ");
+    System.out.print("Introduce un número por teclado y saldra una cadena de numeros fibonacci: ");
     int n1 = Integer.parseInt(System.console().readLine()) ;
-    int n2 = 1;
-    int n3 = n1 + n2;
-    
-    
-    while (n3 < 144){
-      
-    n3 = n1 + n2; 
-    System.out.print( n3 +" ");
-    n1 = n2;
-    n2 = n3;
+
+    switch (n1) {
+      case 1:
+      System.out.print("0");
+      break;
+      case 2: 
+      System.out.print("0 1");
+      break;
+      default: 
+      System.out.print("0 1 ");
       
     }
+    int n2 = 1;
+    int n3 = n1 + n2;
+    while (n1 < 144){
+    
+      n3 = n1 + n2; 
+      System.out.print( n3 +" ");
+      n1 = n2;
+      n2 = n3;
+    
+    ¡}
     
   }
 }
