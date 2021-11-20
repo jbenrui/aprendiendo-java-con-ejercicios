5e174501564e8704ecb3bba982977793d0519fc3
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-11-20 15:21:18 +0100

    Ejercicio 36 TEMA 5

---
+++ TEMA-5/JabrEjercicio36Tema5.java
@@ -0,0 +1,37 @@
+/**
+ * Ejercicio: 36
+ * Escribe un programa que diga si un número introducido por teclado es o no
+ * capicúa. Los números capicúa se leen igual hacia delante y hacia atrás. El
+ * programa debe aceptar números de cualquier longitud siempre que lo permita
+ * el tipo, en caso contrario el ejercicio no se dará por bueno.Se recomienda 
+ * usar long en lugar de int ya que el primero admite números más largos.
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+public class JabrEjercicio36Tema5 {
+  public static void main (String[] args) {
+    System.out.println("El programa nos dira si un numero es capicua o no");
+    System.out.print("Introduce un numero por teclado: ");
+    int numeroIntroducido = Integer.parseInt(System.console().readLine()) ;
+    int numero = numeroIntroducido;
+    int voltear = 0;
+    /**Le doy la vuelta al numero**/
+    while (numero > 0){
+      
+      voltear = (voltear * 10) + (numero %10);
+      numero /= 10;
+    }
+    /**Comparo si el numero dado la vuelta es igual al numero que he introducido**/
+    if (voltear == numeroIntroducido){
+      System.out.print("El "+numeroIntroducido+ " es capicúa");
+    }else{
+    System.out.print("El "+numeroIntroducido+ " no es capicúa");
+    }
+    
+    
+  }
+}
+
+
