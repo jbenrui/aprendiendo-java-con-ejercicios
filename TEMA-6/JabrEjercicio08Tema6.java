010f76ebfcbf4303330f7a742983279506227fbc
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2021-11-28 16:19:47 +0100

    Ejercicio 8 TEMA 6

---
+++ TEMA-6/JabrEjercicio08Tema6.java
@@ -0,0 +1,72 @@
+/**
+ * Ejercicio: 7
+ * Escribe un programa que muestre tres apuestas de la quiniela en tres columnas
+ * para los 14 partidos y el pleno al quince. A continuación se muestra un impre
+ * -so real. Cada columna es una apuesta.
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+public class JabrEjercicio08Tema6 {
+  public static void main (String[] args) {
+    System.out.println("muestre tres apuestas de la quiniela en tres columnas para los 14 partidos y el pleno al quince.");
+    int numAleatorio=0;
+    for (int partidos = 1; partidos <= 15;partidos++){  
+      System.out.printf("%3d. ",partidos);
+      if ( partidos <= 14){
+        for (int apuesta = 0; apuesta < 3; apuesta++){
+          numAleatorio=(int)(Math.random()*6);
+          switch (numAleatorio){
+            case 0:
+              System.out.print("| X |");
+            break;
+            case 1:
+              System.out.print("|  2|");
+            break;
+            case 2:
+              System.out.print("|  2|");
+            break;
+            default:
+              System.out.print("|1  |");
+            break;
+          }
+        }
+        System.out.println("");
+      }else{
+        System.out.printf("PLENO AL %3d. -Local...",partidos);
+        numAleatorio=(int)(Math.random()*4);
+        switch (numAleatorio){
+          case 0:
+            System.out.print("0");
+          break;
+          case 1:
+            System.out.print("1");
+          break;
+            case 2:
+            System.out.print("2");
+          break;
+          default:
+            System.out.print("M");
+          break;
+        }
+        System.out.print("  Visitante...");
+        numAleatorio=(int)(Math.random()*5);
+        switch (numAleatorio){
+          case 0:
+            System.out.print("0");
+          break;
+          case 1:
+            System.out.print("1");
+          break;
+          case 2:
+            System.out.print("2");
+          break;
+          default:
+            System.out.print("M");
+          break;
+        }
+      }
+    }
+  }
+}
