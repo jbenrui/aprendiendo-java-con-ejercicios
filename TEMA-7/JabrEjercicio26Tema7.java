fd32543be846dfaa31eb8cecfc1ce8a5d0fde79e
Jose Antonio Benitez <benitezruiz.joseantonio@gmail.com>
2022-01-10 14:12:21 +0100

    Ejercicio 4 Array Bidimensionales TEMA 7

---
+++ TEMA-7/JabrEjercicio26Tema7.java
@@ -0,0 +1,71 @@
+/**
+ * Ejercicio: 26
+ * Modifica el programa anterior de tal forma que las sumas parciales y la suma
+ * total aparezcan en la pantalla con un pequeño retardo, dando la impresión de
+ * que el ordenador se queda “pensando” antes de mostrar los números.
+ * 
+ * 
+ * @author Jose Antonio Benitez Ruiz
+ * 
+ * 
+ */
+public class JabrEjercicio26Tema7 {
+  public static void main (String[] args) {
+    int[] [] num = new int[4][5]; 
+    int fila;
+    int columna;
+    int[] sumFila = new int[4];
+    int[] sumColumna = new int[5]; 
+    int sumTotal = 0; 
+    System.out.println("Introduzca 20 números:");
+
+    for (fila = 0; fila < 4; fila++) {
+      for (columna = 0; columna < 5; columna++) {
+        System.out.print("> ");
+        num[fila] [columna]= (int)(Math.random()*999)+100;
+        System.out.println(" ");
+
+        sumFila[fila] = sumFila[fila] + num[fila] [columna]; //mientras la fila en la que estemos introduciendo datos no cambie, la posición del array correspondiente irá aumentando
+        sumColumna[columna] = sumColumna[columna] + num[fila] [columna]; //cada vez que se vuelva a una columna la posición del array correspondiente irá aumentando
+        sumTotal = sumTotal + num[fila] [columna]; //la suma total irá incrementando cada vez que introducimos un dato
+      }
+    }
+
+    System.out.println(" ");
+
+    //mostramos los resultados
+    System.out.println("┌───────────────────┬───────────────────┬───────────────────┬───────────────────┬───────────────────┬──────────────────────┐");
+
+    for (fila = 0; fila < 4; fila++) {
+      for (columna = 0; columna < 5; columna++) {
+        System.out.printf("%-1s        %3d        " ,"|" ,num[fila] [columna]);
+      }
+      for (int i = 0; i < 2; i++) {
+        Thread.sleep(1000);
+      }
+      System.out.printf("%-1s %-15s%5d %-1s" ,"|" ,"Sum. fila " + fila + " = " ,sumFila[fila] ,"|");
+      
+
+      System.out.println(" ");
+
+      System.out.println("├───────────────────┼───────────────────┼───────────────────┼───────────────────┼───────────────────┼──────────────────────┤");
+    }
+
+    for (columna = 0; columna < 5; columna++) {
+      for (int i = 0; i < 2; i++) {
+        Thread.sleep(1000);
+      }
+      System.out.printf("%-1s %-11s%5d " ,"|" ,"Sum. C. " + columna + " = " ,sumColumna[columna]);
+    }
+
+    System.out.print("|");
+      for (int i = 0; i < 2; i++) {
+        Thread.sleep(1000);
+      }
+    System.out.printf("     %-6s %5d     %-1s" ,"TOTAL:" ,sumTotal ,"|");
+
+    System.out.println(" ");
+
+    System.out.println("└───────────────────┴───────────────────┴───────────────────┴───────────────────┴───────────────────┴──────────────────────┘");
+  }
+}
