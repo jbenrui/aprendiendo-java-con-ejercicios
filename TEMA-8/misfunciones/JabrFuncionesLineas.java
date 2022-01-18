f3fc280b2de577553e6070e30a16a3f4506e3911
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-01-18 11:00:09 +0100

    Archivo de las Funciones que uso para pintar figuras de caracteres

---
+++ TEMA-8/misfunciones/JabrFuncionesLineas.java
@@ -0,0 +1,19 @@
+package misfunciones;
+
+
+public class JabrFuncionesLineas {
+    public static String linea(int longitud, char extremo, char relleno){
+        System.out.print(extremo);
+        for (int i = 0; i < longitud -2; i++){
+            System.out.print(relleno);
+        }
+        if (longitud > 1){
+            System.out.print(extremo);
+        }
+        return "";
+    }
+    public static String espaciosPorDelante(int numespacios,char relleno){
+        return linea(numespacios,' ',relleno);
+    }
+}
+
