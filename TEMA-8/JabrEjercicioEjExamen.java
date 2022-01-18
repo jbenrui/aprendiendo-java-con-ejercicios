8c7795cb5246d56ef797d4abb2604a1b99273949
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-01-18 10:59:00 +0100

    Ejercicio 4 del de examen del 2do trimestre del año pasado

---
+++ TEMA-8/JabrEjercicioEjExamen.java
@@ -0,0 +1,24 @@
+import misfunciones.JabrFuncionesLineas;
+public class JabrEjercicioEjExamen {
+    public static void main(String[] args){
+        System.out.print("Introduce la altura de la piramide: ");
+        int altura = Integer.parseInt(System.console().readLine());
+        int numespacios = altura;
+        int base = 1;
+        int longitud = 1;
+        char extremo = '*';
+        char relleno = ' ';
+        for (int i = 0; i <= altura -1;i++){
+            System.out.print(JabrFuncionesLineas.espaciosPorDelante(numespacios,relleno));
+            if (base <= altura -1){
+                System.out.println(JabrFuncionesLineas.linea(longitud,extremo,relleno));
+            }else{
+                relleno = '*';
+                System.out.println(JabrFuncionesLineas.linea(longitud,extremo,relleno));
+            }
+            longitud +=2;
+            numespacios--;
+            base++;
+        }
+    }
+}
