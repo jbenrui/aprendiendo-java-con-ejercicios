0143795cb79e024a12dca4ae1d1f7b5040a82c94
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-01-20 09:04:21 +0100

    Ejercicio 15 del TEMA 8

--- TEMA-8/JabrEjercicio15Tema8.java
+++ TEMA-8/JabrEjercicio15Tema8.java
@@ -1,7 +1,7 @@
 import misfunciones.JabrFuncionesMatematicas;
 public class JabrEjercicio15Tema8 {
     public static void main (String[] args) {
-        for (int i = 0; i < 1000; i++){
+        for (int i = 1; i <= 1000; i++){
             if (JabrFuncionesMatematicas.esPrimo(i)){
                 System.out.print(i + " ");
             }
