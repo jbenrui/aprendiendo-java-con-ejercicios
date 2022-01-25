e92a852be7aa2888ce975cf408a9a00e7ef63fad
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-01-25 20:42:13 +0100

    Actualizando y añadiendo funciones de Arrays

--- TEMA-8/misfunciones/JabrFuncionesArrays.java
+++ TEMA-8/misfunciones/JabrFuncionesArrays.java
@@ -28,4 +28,54 @@ public class JabrFuncionesArrays {
         }
         return minimo;
     }
+    public static int maximoArrayInt(int array[]){
+        int max=0;
+        int indice=0;
+        while  (indice < array.length){
+            if (indice == 0 ){
+                max=array[indice];
+            }else if (array[indice] >= max){
+                max=array[indice];
+            }
+            indice++;
+        }
+        return max;
+    }
+    public static int mediaArrayInt(int array[]){
+        int media=0;
+        for (int i = 0; i < array.length;i++){
+            media = media + array[i];
+        }
+        media /= array.length;
+        return media;
+    }
+    public static void estaEnArrayInt(int array[],int numeroIntroducido){
+        int contadorNum = 0;
+        for (int i = 0; i < array.length;i++){
+            if (numeroIntroducido == array[i]){
+                contadorNum++;
+            }
+        }
+        if (contadorNum > 0){
+            System.out.print(" Esta en el array");
+        }else{
+            System.out.print(" NO esta en el array");
+        }
+    }
+    public static int posicionEnArray(int array[],int numeroIntroducido){
+        for (int i = 0; i < array.length;i++){
+            if (numeroIntroducido == array[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int[] volteaArrayInt(int array[]){
+        int [] arrayVolteado = new int [array.length];
+        for (int i= 0; i < arrayVolteado.length; i++){
+            arrayVolteado[i] = array[arrayVolteado.length -1 -i];
+            //Ej.: en un array de 10 elementos, arrayVolteado[0] = array[arrayVolteado.lenght = 10, 10 - 1 - 0 = 9], arrayVolteado[1] = array[8 (i = 2)]
+        }
+        return arrayVolteado;
+    }
 }
