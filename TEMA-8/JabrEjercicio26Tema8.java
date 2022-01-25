09981ef477a65bbb966e647e6e5c012674b13d5c
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-01-25 23:23:52 +0100

    Ejercicio 26 del tema 8

--- TEMA-8/JabrEjercicio26Tema8.java
+++ TEMA-8/JabrEjercicio26Tema8.java
@@ -1,6 +1,8 @@
 import misfunciones.JabrFuncionesArrays;
+import java.util.Scanner;
 public class JabrEjercicio26Tema8 {
     public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
         System.out.print("Introduce el tamaño del array:  ");
         int tamaño = Integer.parseInt(System.console().readLine()) ;
         System.out.print("Introduce minimo valor en el array: ");
@@ -12,7 +14,10 @@ public class JabrEjercicio26Tema8 {
         JabrFuncionesArrays.muestraArray(array);
         System.out.println("_____________________________________________________________");
         System.out.println(" ");
+        System.out.println("Introduce las posiciones que vas ha rotar el array: ");
+        System.out.print("El rango debe ser entre: 0  y "+(tamaño -1)+" .");
+        int numPosiciones = sc.nextInt();
         System.out.println(" ");
-        JabrFuncionesArrays.muestraArray(JabrFuncionesArrays.volteaArrayInt(array));
+        JabrFuncionesArrays.muestraArray(JabrFuncionesArrays.rotaDerechaArrayInt(array,numPosiciones));
     }
 }
