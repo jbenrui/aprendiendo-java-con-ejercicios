33fed4fbe3bf90e16d529eea8bce95282d272059
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-02-07 14:09:47 +0100

    Añadiendo ejercicio 1 tema 9

--- TEMA-9/JabrEjercicio1Tema9/Caballo.java
+++ TEMA-9/JabrEjercicio1Tema9/Caballo.java
@@ -1,21 +1,31 @@
 public class Caballo {
-    int idCaballo;
-    String nomCaballo;
-    int pesoCaballo;
-    int anoNacimiento;
-    int altura;
-    String color;
-    public Caballo(String nomCaballo, int pesoCaballo, int anoNacimiento) {
+    private int idCaballo;
+    private String nomCaballo;
+    private int pesoCaballo;
+    private int anoNacimiento;
+    private int altura;
+    private String color;
+    public Caballo(String nomCaballo, int pesoCaballo, int anoNacimiento, String color) {
         this.nomCaballo = nomCaballo;
         this.pesoCaballo = pesoCaballo;
         this.anoNacimiento = anoNacimiento;
-    }
-    public Caballo(String nomCaballo, String color) {
-        this.nomCaballo = nomCaballo;
         this.color = color;
     }
     public Caballo(String nomCaballo, int altura) {
         this.nomCaballo = nomCaballo;
         this.altura = altura;
+    }
+    public String getNomCaballo() {
+        return this.nomCaballo;
+    }
+    public int getAltura() {
+        return this.altura;
+    }
+    
+    public void cabalga(){
+        System.out.println("Tocotoc Tocotoc Tocotoc");
+    }
+    public void relincha(){
+        System.out.println("Hiieee! Hiiee!");
     }
 }
