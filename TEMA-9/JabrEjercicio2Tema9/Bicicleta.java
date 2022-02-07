e776f4066941babb8b9214a773c28493d3a6aa36
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-02-07 14:10:10 +0100

    Añadiendo ejercicio 2 tema 9

---
+++ TEMA-9/JabrEjercicio2Tema9/Bicicleta.java
@@ -0,0 +1,10 @@
+public class Bicicleta extends Vehiculo{
+    private int pinones;
+    public Bicicleta(int p){
+        super();
+        this.pinones = p;
+    }
+    public void hazCaballito(){
+        System.out.println("Estoy haciendo el caballito.");
+    }
+}
