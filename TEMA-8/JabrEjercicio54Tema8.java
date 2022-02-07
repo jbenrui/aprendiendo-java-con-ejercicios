64263d2d19595aa405add6caed66f4c5c9f41c3b
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-02-07 12:06:25 +0100

    Ejercicio 54 Tema 8

--- TEMA-8/JabrEjercicio54Tema8.java
+++ TEMA-8/JabrEjercicio54Tema8.java
@@ -1,4 +1,3 @@
-import misfunciones.JabrFuncionesMatematicas;
 public class JabrEjercicio54Tema8 {
     public static void main(String[] args){
         int[]a = { 714, 81, 9, 11};
@@ -11,28 +10,27 @@ public class JabrEjercicio54Tema8 {
         System.out.print("introduce un digito: ");
         digito = Integer.parseInt(System.console().readLine()) ;
         System.out.println("ocurrencias (" + ocurrencias(digito,b) + ", a)");
+        System.out.println("______________________________________________");
+        System.out.print("introduce un digito: ");
+        digito = Integer.parseInt(System.console().readLine()) ;
+        System.out.println("ocurrencias (" + ocurrencias(digito,a) + ", a)");
     }
     public static int ocurrencias(int digito, int n) {
         int ocurrencia = 0;
-        int numero = n;
-        int cifra = 0;
-        long voltear = JabrFuncionesMatematicas.voltea(numero);
-        cifra = (int) (voltear % 10);
-        while (voltear > 0){
-            cifra = (int) (voltear % 10);
-            if (cifra == digito){
+        while (n > 0){
+            if ((n % 10) == digito){
                 ocurrencia++;
             }
-            voltear = voltear / 10;
+            n = n / 10;
             
         }
         return ocurrencia;
     }
-    public static int ocurrencias(int digito, int[] n) {
-        int repetido = 0;
-        for (int a : n){
-            repetido += ocurrencias(digito, n);
+    public static int ocurrencias(int digito, int[] a) {
+        int ocurrencia = 0;
+        for (int n : a) {
+            ocurrencia += ocurrencias(digito, n);
         }
-        return repetido;
+            return ocurrencia;
     }
 }
