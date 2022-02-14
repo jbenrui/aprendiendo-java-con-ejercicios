e854571e8f084e00b0aaf9365117a3f1815fb0c8
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-02-14 20:15:53 +0100

    Actualizando ocupaAsientos

--- EjericiosExamenes/ocupaAsientos.java
+++ EjericiosExamenes/ocupaAsientos.java
@@ -1,29 +1,50 @@
 public class ocupaAsientos {
     public static void main(String[] args){
-        int[] a = { 0, 1, 0, 0, 0, 1, 0};
+        int[] a = { 0, 1, 1, 1, 0, 1, 0};
         int espectadores = 3;
         
-        //Busca espacios
-        int huecos = 0;
-        //Comprueba que hay hueco.
-        for(int i = 0; i < a.length; i++){
-            if(a[i] == 0){
-                huecos++;
+        System.out.println(ocupa(a, espectadores));
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + ", ");
+        }
+
+    }
+    public static int ocupa(int[] fila, int espectadores){
+        int espacios = 0;
+        int devuelto = -1;
+        int posicionEspacios = 999;
+        int espaciosJuntos = 0;
+        //comprueba los espacios que hay, y cuenta cuantos espacios hay juntos, si encuentra espacios seguidos, posicionEspacios es la primera butaca
+        for (int i = 0; i < fila.length; i++) {
+            if(fila[i] == 0){
+            espacios++;
+            espaciosJuntos++;
+            if(espaciosJuntos == espectadores && posicionEspacios == 999){
+                posicionEspacios = i - espaciosJuntos;
+            }
+            } else{
+            espaciosJuntos = 0;
             }
         }
-        
-        for(int i = 0; i < a.length; i++){
-            if(((huecos >= espectadores)&&(a[i]==0))&&(espectadores > 0)){
-                a[i]=1;
-                espectadores--;
+        //miramos si hay mas espacios que usuarios
+        if(espectadores <= espacios){
+            devuelto = 0;
+            //Si posicion espacios es diferente al valor dado por defecto, mete los usuarios desde ahí
+            if(posicionEspacios != 999){
+            for (int i = 0; i <= espectadores; i++) {
+                fila[posicionEspacios + i] = 1;
+            }
+            }else{
+            for (int i = 0; i < fila.length; i++) {
+                if(fila[i] == 0 && espectadores >= 1){
+                fila[i] = 1;
+                espectadores --;
+                }
+            }
             }
         }
-
-        for(int i = 0; i < a.length; i++){
-            System.out.print(a[i]);
-        }
-
-
+        return devuelto;
     }
     
 }
